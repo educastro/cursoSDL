ca9b22fc02f2eb794824fc8c5972a84a7ad11d89
Eduardo Castro <eduardocastro91@gmail.com>
2016-07-30 15:05:57 -0300

    user now can add a task, but it is not testes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,25 @@
 class TasksController < ApplicationController
+
+	def create
+		@task = Task.new(task_params)
+		@task.save
+	  	redirect_to '/tasks'
+	end
+	
+	def index
+		@tasks = Task.all
+	end
+
+	def show
+		@task = Task.find(params[:id])
+	end
+
+	def new
+		
+	end
+
+	private
+  	def task_params
+    	params.require(:task).permit(:title)
+  	end
 end
